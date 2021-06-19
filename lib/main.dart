07c527899e4bdd406807764f9ab46a88ccb8d17c
NaufalRizqullah <opalcool8@gmail.com>
2021-06-19 15:49:51 +0800

    [release-v.0.1] setUP

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,4 @@
 import 'dart:ui';
-
-import 'package:draw_aksara/signature.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
@@ -14,36 +12,28 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Offset> _points = <Offset>[];
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
       appBar: AppBar(
         title: Text("Draw Aksara"),
-        backgroundColor: Colors.green,
-      ),
-      body: Container(
-        child: new GestureDetector(
-          onPanUpdate: (DragUpdateDetails details) {
-            setState(() {
-              RenderBox object = context.findRenderObject();
-              Offset _localPosition =
-                  object.globalToLocal(details.localPosition);
-              _points = new List.from(_points)..add(_localPosition);
-            });
-          },
-          onPanEnd: (DragEndDetails details) => _points.add(null),
-          child: new CustomPaint(
-            painter: new Signature(points: _points),
-            size: Size.infinite,
-          ),
+        centerTitle: true,
+        backgroundColor: Colors.green[700],
+        leading: GestureDetector(
+          child: Icon(Icons.menu),
+          onTap: () {},
         ),
+        actions: [
+          Padding(
+            padding: EdgeInsets.only(right: 10.0),
+            child: GestureDetector(
+              child: Icon(Icons.info_outline),
+              onTap: () {},
+            ),
+          ),
+        ],
       ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.clear),
-        onPressed: () => _points.clear(),
-      ),
+      body: Container(),
     );
   }
 }
