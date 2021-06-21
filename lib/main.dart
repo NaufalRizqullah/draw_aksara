c2034aba3df8fd7493e9e7e309d37f2a69e82c72
NaufalRizqullah <opalcool8@gmail.com>
2021-06-21 17:15:49 +0800

    migrae

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,3 @@
-import 'dart:ui';
-
-import 'package:draw_aksara/signature.dart';
 import 'package:flutter/material.dart';
 
 void main() => runApp(new MaterialApp(
@@ -14,8 +11,6 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  List<Offset> _points = <Offset>[];
-
   @override
   Widget build(BuildContext context) {
     return new Scaffold(
@@ -23,27 +18,7 @@ class _HomePageState extends State<HomePage> {
         title: Text("Draw Aksara"),
         backgroundColor: Colors.green,
       ),
-      body: Container(
-        child: new GestureDetector(
-          onPanUpdate: (DragUpdateDetails details) {
-            setState(() {
-              RenderBox object = context.findRenderObject();
-              Offset _localPosition =
-                  object.globalToLocal(details.localPosition);
-              _points = new List.from(_points)..add(_localPosition);
-            });
-          },
-          onPanEnd: (DragEndDetails details) => _points.add(null),
-          child: new CustomPaint(
-            painter: new Signature(points: _points),
-            size: Size.infinite,
-          ),
-        ),
-      ),
-      floatingActionButton: new FloatingActionButton(
-        child: new Icon(Icons.clear),
-        onPressed: () => _points.clear(),
-      ),
+      body: Container(),
     );
   }
 }
