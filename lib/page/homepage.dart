53fdc139e88e2d4e22d69b6ea3e509fd3f694542
NaufalRizqullah <opalcool8@gmail.com>
2021-06-22 20:36:29 +0800

    [released-v.0.2] change name app, adding about page and functionality widget

--- lib/page/homepage.dart
+++ lib/page/homepage.dart
@@ -1,6 +1,7 @@
 import 'dart:ui';
 
 import 'package:draw_aksara/helper/myCustomPainter.dart';
+import 'package:draw_aksara/page/aboutpage.dart';
 import 'package:flutter/material.dart';
 import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter_colorpicker/flutter_colorpicker.dart';
@@ -38,16 +39,6 @@ class _HomePageState extends State<HomePage> {
 
   late double strokeWidth;
 
-  // fungsi untuk load dataDraw berdasarkan index
-  void loadDraw(int indexSaved) {
-    points = List.from(savedDraw[indexSaved]);
-  }
-
-  // fungsi untuk save dataDraw berdasarkan index
-  void saveDraw(int indexSaved) {
-    savedDraw[indexSaved] = List.from(points);
-  }
-
   @override
   void initState() {
     selectedColor = Colors.black;
@@ -62,7 +53,7 @@ class _HomePageState extends State<HomePage> {
     // get size layar
     final double width = MediaQuery.of(context).size.width;
 
-    void selectColor() {
+    void selectColor(BuildContext context) {
       showDialog(
           context: context,
           builder: (BuildContext context) {
@@ -103,7 +94,12 @@ class _HomePageState extends State<HomePage> {
             child: InkResponse(
               child: Icon(Icons.info_outline),
               onTap: () {
-                debugPrint("Hit info");
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => AboutPage(),
+                  ),
+                );
               },
             ),
           ),
@@ -129,224 +125,280 @@ class _HomePageState extends State<HomePage> {
       body: Builder(
         builder: (context) => Column(
           children: [
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.loose,
-                    child: ElevatedButton(
-                      child: Icon(Icons.arrow_back),
-                      onPressed: () {
-                        buttonCarouselController.previousPage(
-                          duration: Duration(seconds: 1),
-                          curve: Curves.linear,
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        onPrimary: Colors.white,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 1,
-                    fit: FlexFit.loose,
-                    child: Center(
-                      child: Text(
-                        "${(currentIndex + 1).toString()}",
-                        style: TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                    ),
-                  ),
-                  Flexible(
-                    flex: 2,
-                    fit: FlexFit.loose,
-                    child: ElevatedButton(
-                      child: Icon(Icons.arrow_forward),
-                      onPressed: () {
-                        buttonCarouselController.nextPage(
-                          duration: Duration(seconds: 1),
-                          curve: Curves.linear,
-                        );
-                      },
-                      style: ElevatedButton.styleFrom(
-                        primary: Colors.blue,
-                        onPrimary: Colors.white,
-                        elevation: 5,
-                        shape: RoundedRectangleBorder(
-                          borderRadius: new BorderRadius.circular(10.0),
-                        ),
-                      ),
-                    ),
-                  ),
-                ],
-              ),
-            ),
-            Flexible(
-              flex: 4,
-              fit: FlexFit.tight,
-              child: Container(
-                child: CarouselSlider(
-                  carouselController: buttonCarouselController,
-                  items: imgAssets
-                      .map(
-                        (e) => Container(
-                          width: MediaQuery.of(context).size.width,
-                          child: Image.asset(
-                            e,
-                            fit: BoxFit.contain,
-                          ),
-                        ),
-                      )
-                      .toList(),
-                  options: CarouselOptions(
-                    initialPage: 0,
-                    enlargeCenterPage: true,
-                    aspectRatio: 16 / 9,
-                    height: MediaQuery.of(context).size.width * 0.8,
-                    viewportFraction: 1.0,
-                    enableInfiniteScroll: false,
-                    onPageChanged: (index, reason) {
-                      this.setState(() {
-                        currentIndex = index;
-                        loadDraw(currentIndex);
-                      });
-                    },
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 4,
-              fit: FlexFit.tight,
-              child: Container(
-                width: width * 0.95,
-                decoration: BoxDecoration(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    boxShadow: [
-                      BoxShadow(
-                        color: Colors.black.withOpacity(0.4),
-                        blurRadius: 5.0,
-                        spreadRadius: 5.0,
-                      ),
-                    ]),
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                child: GestureDetector(
-                  onPanDown: (details) {
-                    this.setState(() {
-                      points.add(details.localPosition);
-                    });
-                  },
-                  onPanUpdate: (details) {
-                    this.setState(() {
-                      this.setState(() {
-                        points.add(details.localPosition);
-                      });
-                    });
-                  },
-                  onPanEnd: (details) {
-                    this.setState(() {
-                      points.add(Offset(0, 0));
-                    });
-                  },
-                  child: ClipRRect(
-                    borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                    child: CustomPaint(
-                      painter: MyCustomPainter(
-                          points: points,
-                          setColor: selectedColor,
-                          strokeWidth: strokeWidth),
-                    ),
-                  ),
-                ),
-              ),
-            ),
-            Flexible(
-              flex: 1,
-              fit: FlexFit.tight,
-              child: Container(
-                margin: EdgeInsets.only(top: 10, bottom: 10),
-                decoration: BoxDecoration(
-                  color: Colors.white,
-                  borderRadius: BorderRadius.all(Radius.circular(5.0)),
-                ),
-                width: width * 0.95,
-                child: Row(
-                  children: [
-                    IconButton(
-                      icon: Icon(Icons.cloud_download_outlined),
-                      onPressed: () {},
-                    ),
-                    IconButton(
-                      icon: Icon(
-                        Icons.save,
-                      ),
-                      onPressed: () {
-                        showActionSnackBar(context, "Draw Saved!");
-                        this.setState(() {
-                          saveDraw(currentIndex);
-                        });
-                      },
-                    ),
-                    IconButton(
-                        icon: Icon(
-                          Icons.color_lens,
-                        ),
-                        onPressed: () {
-                          selectColor();
-                        }),
-                    Expanded(
-                        child: Slider(
-                      min: 1.0,
-                      max: 20.0,
-                      activeColor: selectedColor,
-                      value: strokeWidth,
-                      onChanged: (newValue) {
-                        this.setState(() {
-                          strokeWidth = newValue;
-                        });
-                      },
-                    )),
-                    IconButton(
-                        icon: Icon(Icons.layers_clear),
-                        onPressed: () {
-                          this.setState(() {
-                            points.clear();
-                          });
-                        }),
-                  ],
-                ),
-              ),
-            ),
+            buttonController(
+                context: context,
+                buttonCarouselController: buttonCarouselController,
+                currentIndex: currentIndex),
+            imageCarouselSlider(
+                imgAssets: imgAssets,
+                buttonCarouselController: buttonCarouselController,
+                context: context),
+            drawCanvas(
+                points: points,
+                selectedColor: selectedColor,
+                strokeWidth: strokeWidth,
+                width: width),
+            controllerCanvas(
+                points: points,
+                selectColor: selectColor,
+                strokeWidth: strokeWidth,
+                selectedColor: selectedColor,
+                width: width),
           ],
         ),
       ),
     );
   }
-}
 
-// function snackbar
-void showActionSnackBar(BuildContext context, String msg) {
-  final snackBar = SnackBar(
-    content: Text(msg.toString(), style: TextStyle(fontSize: 16)),
-    action: SnackBarAction(
-      label: "Dismiss",
-      onPressed: () {},
-    ),
-  );
+  // function snackbar
+  void showActionSnackBar(BuildContext context, String msg) {
+    final snackBar = SnackBar(
+      content: Text(msg.toString(), style: TextStyle(fontSize: 16)),
+      action: SnackBarAction(
+        label: "Dismiss",
+        onPressed: () {},
+      ),
+    );
 
-  ScaffoldMessenger.of(context)
-    ..removeCurrentSnackBar()
-    ..showSnackBar(snackBar);
+    ScaffoldMessenger.of(context)
+      ..removeCurrentSnackBar()
+      ..showSnackBar(snackBar);
+  }
+
+  // fungsi untuk load dataDraw berdasarkan index
+  void loadDraw(int indexSaved) {
+    points = List.from(savedDraw[indexSaved]);
+  }
+
+  // fungsi untuk save dataDraw berdasarkan index
+  void saveDraw(int indexSaved) {
+    savedDraw[indexSaved] = List.from(points);
+  }
+
+  // button controller widget
+  Widget buttonController(
+          {required BuildContext context,
+          required CarouselController buttonCarouselController,
+          required int currentIndex}) =>
+      Flexible(
+        flex: 1,
+        fit: FlexFit.tight,
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          children: [
+            Flexible(
+              flex: 2,
+              fit: FlexFit.loose,
+              child: ElevatedButton(
+                child: Icon(Icons.arrow_back),
+                onPressed: () {
+                  buttonCarouselController.previousPage(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.linear,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 1,
+              fit: FlexFit.loose,
+              child: Center(
+                child: Text(
+                  "${(currentIndex + 1).toString()}",
+                  style: TextStyle(
+                    fontSize: 20,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+              ),
+            ),
+            Flexible(
+              flex: 2,
+              fit: FlexFit.loose,
+              child: ElevatedButton(
+                child: Icon(Icons.arrow_forward),
+                onPressed: () {
+                  buttonCarouselController.nextPage(
+                    duration: Duration(seconds: 1),
+                    curve: Curves.linear,
+                  );
+                },
+                style: ElevatedButton.styleFrom(
+                  primary: Colors.blue,
+                  onPrimary: Colors.white,
+                  elevation: 5,
+                  shape: RoundedRectangleBorder(
+                    borderRadius: new BorderRadius.circular(10.0),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      );
+
+  // image slider carousel widget
+  Widget imageCarouselSlider(
+          {required List<String> imgAssets,
+          required CarouselController buttonCarouselController,
+          required BuildContext context}) =>
+      Flexible(
+        flex: 4,
+        fit: FlexFit.tight,
+        child: Container(
+          child: CarouselSlider(
+            carouselController: buttonCarouselController,
+            items: imgAssets
+                .map(
+                  (e) => Container(
+                    width: MediaQuery.of(context).size.width,
+                    child: Image.asset(
+                      e,
+                      fit: BoxFit.contain,
+                    ),
+                  ),
+                )
+                .toList(),
+            options: CarouselOptions(
+              initialPage: 0,
+              enlargeCenterPage: true,
+              aspectRatio: 16 / 9,
+              height: MediaQuery.of(context).size.width * 0.8,
+              viewportFraction: 1.0,
+              enableInfiniteScroll: false,
+              onPageChanged: (index, reason) {
+                this.setState(() {
+                  currentIndex = index;
+                  loadDraw(currentIndex);
+                });
+              },
+            ),
+          ),
+        ),
+      );
+
+  // canvas widget
+  Widget drawCanvas(
+          {required double width,
+          required List<Offset> points,
+          required Color selectedColor,
+          required double strokeWidth}) =>
+      Flexible(
+        flex: 4,
+        fit: FlexFit.tight,
+        child: Container(
+          width: width * 0.95,
+          decoration: BoxDecoration(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              boxShadow: [
+                BoxShadow(
+                  color: Colors.black.withOpacity(0.4),
+                  blurRadius: 5.0,
+                  spreadRadius: 5.0,
+                ),
+              ]),
+          margin: EdgeInsets.only(top: 10, bottom: 10),
+          child: GestureDetector(
+            onPanDown: (details) {
+              this.setState(() {
+                points.add(details.localPosition);
+              });
+            },
+            onPanUpdate: (details) {
+              this.setState(() {
+                this.setState(() {
+                  points.add(details.localPosition);
+                });
+              });
+            },
+            onPanEnd: (details) {
+              this.setState(() {
+                points.add(Offset(0, 0));
+              });
+            },
+            child: ClipRRect(
+              borderRadius: BorderRadius.all(Radius.circular(5.0)),
+              child: CustomPaint(
+                painter: MyCustomPainter(
+                    points: points,
+                    setColor: selectedColor,
+                    strokeWidth: strokeWidth),
+              ),
+            ),
+          ),
+        ),
+      );
+
+  // controller canvas widget
+  Widget controllerCanvas(
+          {required double width,
+          required Color selectedColor,
+          required double strokeWidth,
+          required List<Offset> points,
+          required selectColor}) =>
+      Flexible(
+        flex: 1,
+        fit: FlexFit.tight,
+        child: Container(
+          margin: EdgeInsets.only(top: 10, bottom: 10),
+          decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.all(Radius.circular(5.0)),
+          ),
+          width: width * 0.95,
+          child: Row(
+            children: [
+              IconButton(
+                icon: Icon(Icons.cloud_download_outlined),
+                onPressed: () {},
+              ),
+              IconButton(
+                icon: Icon(
+                  Icons.save,
+                ),
+                onPressed: () {
+                  showActionSnackBar(context, "Draw Saved!");
+                  this.setState(() {
+                    saveDraw(currentIndex);
+                  });
+                },
+              ),
+              IconButton(
+                  icon: Icon(
+                    Icons.color_lens,
+                  ),
+                  onPressed: () {
+                    selectColor(context);
+                  }),
+              Expanded(
+                  child: Slider(
+                min: 1.0,
+                max: 20.0,
+                activeColor: selectedColor,
+                value: strokeWidth,
+                onChanged: (newValue) {
+                  this.setState(() {
+                    strokeWidth = newValue;
+                  });
+                },
+              )),
+              IconButton(
+                  icon: Icon(Icons.layers_clear),
+                  onPressed: () {
+                    this.setState(() {
+                      points.clear();
+                    });
+                  }),
+            ],
+          ),
+        ),
+      );
 }
